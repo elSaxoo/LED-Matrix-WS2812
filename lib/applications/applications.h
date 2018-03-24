b77ccea561b8342f2c94aa2a8e90ec27b9fb389c
elSaxoo <elsaxoo@gmail.com>
2018-03-24 11:56:08 +0100

    delete old font array and old scrolltext

--- lib/applications/applications.h
+++ lib/applications/applications.h
@@ -7,24 +7,14 @@
 namespace LEDArrangement
 {
 
-// Print-Funktion von elSaxoo
-void show_string(LEDMatrix &mat, const String text, const CRGB color = CRGB(0, 0, 255), const int8_t shift_horizontal = 0, const bool begin_right = false, const CRGB background = CRGB(0, 0, 0));
-
-// Überladung von print_String für CHSV Farbe
-void show_string(LEDMatrix &mat, const String text, const CHSV color, const int8_t shift_horizontal = 0, const bool begin_right = false, const CHSV background = CHSV(0, 0, 0));
-
-// Print-Funktion von SvenJupiter
 void print_char(LEDMatrix &mat, const char character, const CRGB color = CRGB(255, 0, 0), const CRGB background = CRGB(0, 0, 0));
 
-// Print-Funktion von SvenJupiter
 // Überladung von print_char für CHSV Farbe
 void print_char(LEDMatrix &mat, const char character, const CHSV color, const CHSV background = CHSV(0, 0, 0));
 
-// Print-Funktion von SvenJupiter
 // Überladung von print_string für CHSV Farbe
 void print_string(LEDMatrix &mat, const char *const text, const CRGB color = CRGB(255, 0, 0), const CRGB background = CRGB(0, 0, 0), const UINT_8 space_between_characters = 1);
 
-// Print-Funktion von SvenJupiter
 // Überladung von print_string für CHSV Farbe
 void print_string(LEDMatrix &mat, const char *const text, const CHSV color, const CHSV background = CHSV(0, 0, 0), const UINT_8 space_between_characters = 1);
 
